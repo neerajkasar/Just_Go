9c28bf65dcd3a1d6f5dc3c6452358c8c680eebd7
NeerajKasar <educationpov@gmail.com>
2025-06-22 14:14:41 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+app.get('/',(req,res) => {
+    res.send('Hello Develper');
+})
+
+module.exports = app;
